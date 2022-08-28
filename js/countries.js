f7c11b7ad86333ce0fbc50d69d2998d56ec71080
unknown <mehedi15-2995@diu.edu.bd>
2022-08-28 21:23:18 +0600

    first commit

---
+++ js/countries.js
@@ -0,0 +1,54 @@
+const loadCountries =()=>{
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res=>res.json())
+    .then(data=>displayCountries(data))
+    
+}
+
+
+const displayCountries =countries =>{
+    // for(const country of countries){
+    //     console.log(country)
+    // }
+    const countriesContainer = document.getElementById('countries-container');
+
+
+
+    countries.forEach(country=>{
+        const countryDiv = document.createElement('div');
+        countryDiv.classList.add('country');
+        
+        countryDiv.innerHTML=`
+            <h3>Name:${country.name.common}</h3>
+            <p>Capital: ${country.capital}</p>
+            <button onclick="loadCountryDetail('${country.cca2}')">Details</button>
+           
+        
+        `;
+        countriesContainer.appendChild(countryDiv);
+    })
+        
+    
+}
+const loadCountryDetail=(code)=>{
+    // https://restcountries.com/v3.1/alpha/{code}
+
+    const url =`https://restcountries.com/v3.1/alpha/${code}`;
+
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>displayCountryDetail(data[0]))
+}
+
+
+const displayCountryDetail = country=>{
+    const countryDetail = document.getElementById('country-detail');
+    countryDetail.innerHTML=`
+            <h2>Details:${country.name.common}</h2>
+            <img src="${country.flags.png}"></img>
+    
+    `
+
+
+}
+loadCountries()
---
+++ js/random-user.js
@@ -0,0 +1,34 @@
+const loadUsers = () => {
+
+    fetch('https://randomuser.me/api/?results=10')
+    .then(res => res.json())
+    .then(data =>displayUsers(data.results))
+
+}
+
+
+
+const displayUsers= users=>{
+    const usersContainer = document.getElementById('users-container');
+    for(const user of users){
+        console.log(user)
+        const userDiv = document.createElement('div');
+        userDiv.classList.add('user');
+        userDiv.innerHTML=`
+        <h3>User Name: ${user.name.first} ${user.name.last}</h3>
+        <p><b>Age:</b> ${user.dob.age}</p>
+        <p>User Id: ${user.id.value}</p>
+
+        <p>E-mail:${user.email}</p>
+        <p>User Location: ${user.location.city} ,${user.location.country}</p>
+        <p>Gender: ${user.gender}</p>
+        
+        `;
+        usersContainer.appendChild(userDiv);
+        
+    }
+    
+    
+    
+}
+loadUsers();
